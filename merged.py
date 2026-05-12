ef89a6143213661429f6c81b92d1d56bfc3b3ec7
Aakash Dhotre <110091530+Dhotre12@users.noreply.github.com>
2026-05-12 10:54:34 +0530

    Update merged.py

--- merged.py
+++ merged.py
@@ -22,11 +22,7 @@ import shap
 import matplotlib.pyplot as plt
 
 # --- NEW IMPORTS FOR TRANSLATION ---
-# from googletrans import Translator # Requires: pip install googletrans==4.0.0-rc1
-from deep_translator import GoogleTranslator
-
-# To translate:
-result = GoogleTranslator(source='auto', target='en').translate('texto a traducir')
+from googletrans import Translator # Requires: pip install googletrans==4.0.0-rc1
 
 # LIME: Used for local, instance-specific explanations.
 # Wrapped in a try-except block to ensure the app doesn't crash if LIME isn't installed.
@@ -69,7 +65,7 @@ def translate_text(text, dest_lang):
     # --------------------------------------------
         
     try:
-       # translator = Translator()
+        translator = Translator()
         translation = translator.translate(text_str, dest=dest_lang)
         return translation.text
     except Exception as e:
